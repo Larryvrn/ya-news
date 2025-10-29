97143c991be29be955fe61117917a53b192ad417
Sergey Udin <larryvrn2@gmail.com>
2025-10-30 00:00:49 +0300

    v.0.2.0 - first version of fully formatted tests for project review

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,38 +1,65 @@
-# news/tests/test_content.py
-from datetime import datetime, timedelta
+"""
+Модуль тестирования контента и отображения приложения новостей.
 
-from django.conf import settings
-from django.test import TestCase
-from django.urls import reverse
-from django.contrib.auth import get_user_model
-from django.utils import timezone
+Содержит тесты для проверки:
+- Количества новостей на главной странице согласно настройкам
+- Порядка сортировки новостей от новых к старым
+- Порядка сортировки комментариев от старых к новым
+- Доступности форм комментариев для авторизованных пользователей
+- Корректности передачи данных в контекст шаблонов
+"""
+from datetime import datetime, timedelta  # type: ignore
 
-from news.models import News, Comment
-from news.forms import CommentForm
+from django.conf import settings  # type: ignore
+from django.test import TestCase  # type: ignore
+from django.urls import reverse  # type: ignore
+from django.contrib.auth import get_user_model  # type: ignore
+from django.utils import timezone  # type: ignore
+
+from news.models import News, Comment  # type: ignore
+from news.forms import CommentForm  # type: ignore
 
 # Текущая дата.
 today = datetime.today()
-# Вчера.
-yesterday = today - timedelta(days=1)
-# Завтра.
-tomorrow = today + timedelta(days=1) 
 
 User = get_user_model()
 
 
 class TestHomePage(TestCase):
-    """Класс тестирования домашней страницы"""
+    """
+    Тестирование домашней страницы приложения новостей.
+
+    Проверяет базовую функциональность главной страницы, включая
+    ограничение количества отображаемых новостей и правильность их сортировки.
+    """
+
     # Вынесем ссылку на домашнюю страницу в атрибуты класса.
     HOME_URL = reverse('news:home')
 
     @classmethod
     def setUpTestData(cls):
+        """
+        Подготавливает тестовые данные для домашней страницы.
+
+        Создает количество новостей, превышающее лимит отображения,
+        для проверки корректности пагинации.
+        """
         News.objects.bulk_create(
-            News(title=f'Новость {index}', text='Просто текст.', date=today - timedelta(days=index))
+            News(
+                title=f'Новость {index}',
+                text='Просто текст.',
+                date=today - timedelta(days=index)
+            )
             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
         )
 
     def test_news_count(self):
+        """
+        Проверяет ограничение количества новостей на главной странице.
+
+        Убеждается, что количество отображаемых новостей не превышает
+        значение NEWS_COUNT_ON_HOME_PAGE из настроек проекта.
+        """
         # Загружаем главную страницу.
         response = self.client.get(self.HOME_URL)
         # Код ответа не проверяем, его уже проверили в тестах маршрутов.
@@ -44,6 +71,12 @@ class TestHomePage(TestCase):
         self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
 
     def test_news_order(self):
+        """
+        Проверяет порядок сортировки новостей на главной странице.
+
+        Убеждается, что новости отсортированы по дате в порядке убывания
+        (сначала самые свежие, затем более старые).
+        """
         response = self.client.get(self.HOME_URL)
         object_list = response.context['object_list']
         # Получаем даты новостей в том порядке, как они выведены на странице.
@@ -55,9 +88,21 @@ class TestHomePage(TestCase):
 
 
 class TestDetailPage(TestCase):
-    """Класс тестирования страницы новости"""
+    """
+    Тестирование страницы детального просмотра новости.
+
+    Проверяет функциональность страницы отдельной новости, включая
+    отображение комментариев и доступность форм для разных пользователей.
+    """
+
     @classmethod
     def setUpTestData(cls):
+        """
+        Подготавливает тестовые данные для страницы новости.
+
+        Создает тестовую новость, пользователя и набор комментариев
+        с разными датами создания для проверки сортировки.
+        """
         cls.news = News.objects.create(
             title='Тестовая новость', text='Просто текст.'
         )
@@ -78,6 +123,12 @@ class TestDetailPage(TestCase):
             comment.save()
 
     def test_comments_order(self):
+        """
+        Проверяет порядок сортировки комментариев на странице новости.
+
+        Убеждается, что комментарии отсортированы по дате создания
+        в порядке возрастания (сначала самые старые, затем новые).
+        """
         response = self.client.get(self.detail_url)
         # Проверяем, что объект новости находится в словаре контекста
         # под ожидаемым именем - названием модели.
@@ -94,10 +145,22 @@ class TestDetailPage(TestCase):
         self.assertEqual(all_timestamps, sorted_timestamps)
 
     def test_anonymous_client_has_no_form(self):
+        """
+        Проверяет отсутствие формы комментария для анонимных пользователей.
+
+        Убеждается, что анонимные пользователи не видят форму
+        для добавления комментариев на странице новости.
+        """
         response = self.client.get(self.detail_url)
         self.assertNotIn('form', response.context)
 
     def test_authorized_client_has_form(self):
+        """
+        Проверяет наличие формы комментария для авторизованных пользователей.
+
+        Убеждается, что авторизованные пользователи видят форму
+        для добавления комментариев правильного типа на странице новости.
+        """
         # Авторизуем клиент при помощи ранее созданного пользователя.
         self.client.force_login(self.author)
         response = self.client.get(self.detail_url)
